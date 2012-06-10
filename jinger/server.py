80d2ef39945a4ab95b2fec14d0ac90f683342f41
Vineet <naikvin@gmail.com>
2012-06-10 21:47:40 +0530

    Refactor server.py code
    * Replace unnecessary classes with functions
    * Use mimetypes module from std lib for guessing the Content Type
    * Update tests

--- jinger/server.py
+++ jinger/server.py
@@ -1,8 +1,25 @@
 import os
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from jinja2 import Environment, FileSystemLoader
+import mimetypes
 
 from jinger.config import get_config
+
+
+_env = None
+
+
+def get_env(rootpath=os.getcwd()):
+    """
+    Function for getting the jinja2 template environment instance so
+    that it's loaded only once.
+    """
+    global _env
+    if _env is None:
+        conf = get_config(rootpath)
+        templatepath = os.path.join(rootpath, conf['sourcedir'])
+        _env = Environment(loader=FileSystemLoader(templatepath))
+    return _env
 
 
 class Http404(Exception):
@@ -12,107 +29,53 @@ class Http404(Exception):
 class JingerHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        rootpath = os.getcwd()
+        self.path = '/index.html' if self.path == '/' else self.path
+        ctype = mimetypes.guess_type(self.path)[0]
         try:
-            content_handler = web_content_factory(self.path)
             self.send_response(200)
-            self.send_header('Content-type', content_handler.content_type)
+            self.send_header('Content-type', ctype)
             self.end_headers()
-            self.wfile.write(content_handler.get_content())
+            self.wfile.write(get_content(self.path, rootpath, ctype))
         except Http404:
             self.send_error(404, 'Template not found for path: %s' % self.path)
 
 
-def web_content_factory(path):
-    """
-    Function to sniff the content type of the file requested and
-    return an instance of the correct WebContent subclass
-    """
-    if path.endswith(('/', '.html')):
-        return HtmlContent(path)
-    if path.endswith(('.png', '.jpeg', '.jpg', '.gif')):
-        return ImageContent(path)
-    if path.endswith('.css'):
-        return CSSContent(path)
-    if path.endswith('.js'):
-        return JSContent(path)
-    if path.endswith('favicon.ico'):
-        return Favicon(path)
+def get_content(path, rootpath=os.getcwd(), ctype=None):
+    ctype = mimetypes.guess_type(path)[0] if ctype is None else ctype
+    if ctype == 'text/html':
+        return compiled_template_content(path, rootpath)
+    elif webasset_exists(rootpath, path):
+        return webassets_content(path, rootpath)
+    elif ctype == 'image/x-icon':
+        return ''
+    else:
+        raise Http404
 
 
-class WebContent(object):
-    
-    content_type = 'plain/text'
-
-    def __init__(self, path, rootpath=os.getcwd()):
-        self.path = path
-        self.rootpath = rootpath
-
-
-class JinjaTemplateContentMixin(object):
-
-    env = None
-    
-    @classmethod
-    def get_env(cls, rootpath):
-        if cls.env is None:
-            conf = get_config(rootpath)
-            templatepath = os.path.join(rootpath, conf['sourcedir'])
-            cls.env = Environment(loader=FileSystemLoader(templatepath))
-        return cls.env
-
-    def get_content(self):
-        templatepath = self.path.strip('/')
-        try:
-            env = self.__class__.get_env(self.rootpath)
-            return env.get_template(templatepath).render()
-        except IOError:
-            raise Http404
+def compiled_template_content(path, rootpath):
+    templatepath = path.strip('/')
+    try:
+        env = get_env(rootpath)
+        return env.get_template(templatepath).render()
+    except IOError:
+        raise Http404
 
 
-class HtmlContent(WebContent, JinjaTemplateContentMixin):
-
-    content_type = 'text/html'
-
-    def __init__(self, path, *args, **kwargs):
-        super(HtmlContent, self).__init__(path, *args, **kwargs)
-        self.path = '/index.html' if self.path == '/' else self.path
-
-
-class WebAssetsContentMixin(object):
-    
-    def get_content(self):
-        try:
-            filepath = os.path.join(self.rootpath, 'webassets', self.path.strip('/'))
-            with open(filepath) as f:
-                return f.read()
-        except IOError:
-            raise Http404
+def webassets_content(path, rootpath):
+    try:
+        with open(webasset_path(rootpath, path)) as f:
+            return f.read()
+    except IOError:
+        raise Http404
 
 
-class CSSContent(WebContent, WebAssetsContentMixin):    
-    content_type = 'text/css'
+def webasset_path(rootpath, path):
+    return os.path.join(rootpath, 'webassets', path.strip('/'))
 
 
-class JSContent(WebContent, WebAssetsContentMixin):
-    content_type = 'text/javascript'
-
-
-class ImageContent(WebContent, WebAssetsContentMixin):
-    
-    @property
-    def content_type(self):
-        ext = os.path.splitext(self.path)[1]
-        return 'image/%s' % ext.lower().strip('.')
-
-
-class Favicon(WebContent, WebAssetsContentMixin):
-    content_type = 'image/x-icon'
-
-    def get_content(self):
-        try:
-            return WebAssetsContentMixin.get_content(self)
-        except Http404:
-            return ''
+def webasset_exists(rootpath, path):
+    return os.path.exists(webasset_path(rootpath, path))
 
 
 def startserver(port):
--- jinger/test/test_server.py
+++ jinger/test/test_server.py
@@ -1,7 +1,7 @@
 import os
 
 from jinger.test import JingerPlaygroundTest, DIR_PLAYGROUND
-from jinger import server
+from jinger.server import get_content, webasset_path
 from jinger.site import create_empty_site
 
 class WebContentTest(JingerPlaygroundTest):
@@ -10,51 +10,27 @@ class WebContentTest(JingerPlaygroundTest):
         super(WebContentTest, self).setUp()
         create_empty_site('mysite', DIR_PLAYGROUND)
         self.site = os.path.abspath(os.path.join(DIR_PLAYGROUND, 'mysite'))
-    
 
-    def test_web_content_factory(self):
-        inst_map = {
-            server.HtmlContent: ['/index.html', '/products/index.html'],
-            server.CSSContent: ['/css/styles.css'],
-            server.JSContent: ['/js/main.js'],
-            server.ImageContent: ['/images/star.jpg'],
-            server.Favicon: ['/favicon.ico'],
-            }
-
-        for C, paths in inst_map.iteritems():
-            for p in paths:
-                inst = server.web_content_factory(p)
-                self.assertIsInstance(inst, C)
-
-
-    def test_html_content(self):        
         with open(os.path.join(self.site, 'templates', 'index.html'), 'w') as f:
             f.write('<!DOCTYPE html>')
-        content = server.HtmlContent('/index.html', rootpath=self.site)
-        self.assertEqual(content.content_type, 'text/html')
-        self.assertEqual(content.get_content(), '<!DOCTYPE html>')
 
-
-    def test_css_content(self):
-        with open(os.path.join(self.site, 'webassets', 'styles.css'), 'w') as f:
+        with open(webasset_path(self.site, 'styles.css'), 'w') as f:
             f.write('html, body {}')
-        content = server.CSSContent('/styles.css', rootpath=self.site)
-        self.assertEqual(content.content_type, 'text/css')
-        self.assertEqual(content.get_content(), 'html, body {}')
 
-
-    def test_js_content(self):
-        with open(os.path.join(self.site, 'webassets', 'main.js'), 'w') as f:
+        with open(webasset_path(self.site, 'main.js'), 'w') as f:
             f.write('var MYSITE = {};')
-        content = server.JSContent('/main.js', rootpath=self.site)
-        self.assertEqual(content.content_type, 'text/javascript')
-        self.assertEqual(content.get_content(), 'var MYSITE = {};')
 
-
-    def test_image_content(self):
-        with open(os.path.join(self.site, 'webassets', 'logo.png'), 'w') as f:
+        with open(webasset_path(self.site, 'logo.png'), 'w') as f:
             f.write('100011')
-        content = server.ImageContent('/logo.png', rootpath=self.site)
-        self.assertEqual(content.content_type, 'image/png')
-        self.assertEqual(content.get_content(), '100011')
+
+
+    def test_get_content(self):
+        content = get_content('/index.html', rootpath=self.site)
+        self.assertEqual(content, '<!DOCTYPE html>')
+        content = get_content('/styles.css', rootpath=self.site)
+        self.assertEqual(content, 'html, body {}')
+        content = get_content('/main.js', rootpath=self.site)
+        self.assertEqual(content, 'var MYSITE = {};')
+        content = get_content('/logo.png', rootpath=self.site)
+        self.assertEqual(content, '100011')
 
